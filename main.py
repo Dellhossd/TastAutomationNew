99278e2bfd6203577ce45fd1536198b8df9d475c
Dellhossd <dh5367480@gmail.com>
2023-02-21 11:48:52 -0500

    Project Setup

---
+++ main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Dell")
+if 10>5:
+    print('ten is bigger then five')
+if 5<9:
+    print("Five is lessen then Nine")
+x =5
+y =9
+print(x+y)
+x =10
+y =("Mark")
+print(str(x)+y)
+
+x =10
+y =("Mark")
+print(y+str(x))
+name =input('What is your name?')
+print("Hello" + name)
+Birth_Year =input("Enter your Birth year:")
+age =2020 - int(birth_year)
+print(age)
